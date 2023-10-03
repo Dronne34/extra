0f05f5291816c394c2b20a953c0f7d8c2af794d0
Dronne34 <ozy23as@gmail.com>
2023-10-03 19:02:16 +0300

    extra_aur.sh update!

--- extra_aur.sh
+++ extra_aur.sh
@@ -5,7 +5,7 @@ echo "Installing packages from pklist!" & sleep 1
 tput sgr0
 echo
 #echo -e "\n"
-
+DIR=~/Github/extra
 BLUE='\033[38;5;4m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -20,7 +20,7 @@ GREEN='\033[0;32m'
 # tput setaf 7 = gray
 # tput setaf 8 = light blue
 
-# sudo pacman -S --needed - < pkglist.txt
+sudo pacman -S --needed - < "${PWD%/}/pklist.txt"
 echo  -e $GREEN"Installing packages done! \n"  & sleep 1
 
 
@@ -41,10 +41,10 @@ echo  -e $GREEN"Installing packages done! \n"  & sleep 1
 #Install from AUR
 echo -e $RED"Install from AUR"  & sleep 1
 pkaur=(
-pulseaudio-nextsink
-sublime-text-4
-brave-bin
-downgrade
+# pulseaudio-nextsink
+# sublime-text-4
+# brave-bin
+# downgrade
 reddio
 )
 #AUR Loop through the package list and install if not already installed
@@ -61,14 +61,20 @@ echo -e $BLUE"Install from AUR done" & sleep 1
 
 echo -e $BLUE"Git Clone oh-my-zsh and tool" & sleep 1
 
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/Dronne34/config ~/.config
-git clone https://github.com/Dronne34/.fonts
-git clone https://github.com/Dronne34/home ~/.home
-cp -arf ~/.home/. ~/ && rm -rf ~/.home
-# sh papirus.sh
+# git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# git clone https://github.com/Dronne34/config ~/.config
+# git clone https://github.com/Dronne34/.fonts
+# git clone https://github.com/Dronne34/home ~/.home
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+# ~/.fzf/install
+# cp -arf ~/.home/. ~/ && rm -rf ~/.home
+cd $DIR
+echo "Script executed from: ${PWD}"
+BASEDIR=$(dirname $0)
+echo "Script location: ${BASEDIR}"
+./papirus.sh
 echo -e $BLUE"Install oh-my-zsh setup done!" & sleep 1
