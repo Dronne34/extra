580b931a7507a8721105083e816b1755a6a5e136
Dronne34 <ozy23as@gmail.com>
2023-11-18 15:47:13 +0000

    modified:   extra_aur.sh

--- extra_aur.sh
+++ extra_aur.sh
@@ -134,17 +134,19 @@ sudo make install
 echo -e "Install st-0.9 done!" & sleep 1
 
 cd $DIR
-### clone and install st-0.9
+### clone and install dwmblocks
 git clone --depth=1 https://github.com/Dronne34/dwmblocks
 cd $DIR_ST  
 sudo make install
 echo -e "Install dwmblocks done!" & sleep 1
 
 cd $DIR
+
 ### root
-# rm -rf st-0.9/ dmenu/ dwm-6.4.1/ dwmblocks/
+cp -rf st-0.9/ dmenu/ dwm-6.4.1/ dwmblocks/ ~/Github
+rm -rf st-0.9/ dmenu/ dwm-6.4.1/ dwmblocks/
 sudo cp -rf $DIR/bin/30-touchpad.conf /usr/share/X11/xorg.conf.d/
-sudo cp -rf $DIR/bin/bluetooth-disable-before-sleep.service /etc/systemd/system/
+#sudo cp -rf $DIR/bin/bluetooth-disable-before-sleep.service /etc/systemd/system/
 sudo cp -rf $DIR/bin/20-intel.conf /etc/X11/xorg.conf.d/
 sudo cp -rf $DIR/bin/pacman.conf /etc/
 sudo cp -rf $DIR/bin/backlight.rules /etc/udev/rules.d/
